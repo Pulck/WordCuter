c8d52c89df7dcf29b9b10da206c1579d8c331ed5
Colick <953687449@qq.com>
2019-03-10 00:08:53 +0800

    Initial Commit

---
+++ WordCuter/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  WordCuter
+//
+//  Created by 梁琛 on 2019/3/10.
+//  Copyright © 2019 liangc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
